d1d2ac0f019b024cef0c3698c61d9e47716ef305
Matteo Norrito <matteo.norrito@outlook.com>
2023-09-14 17:29:17 +0200

    firebase: login

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -5,7 +5,11 @@ import SearchBar from "../SearchBar";
 import styles from "./Todos.module.scss";
 import { rejects } from "assert";
 
-const Todos = () => {
+type props = {
+  isLogged: boolean;
+};
+
+const Todos = ({ isLogged }: props) => {
   const [todoList, setTodoList] = useState<todoModel[]>([]);
   const [todo, setTodo] = useState<string>("");
   const [alert, setAlert] = useState<boolean>(false);
@@ -20,12 +24,28 @@ const Todos = () => {
         alert={alert}
         setAlert={setAlert}
       />
-      {todoList.length ? (
-        todoList.map((data, i) => {
-          return <Card key={i} data={data} />;
-        })
+      {isLogged ? (
+        todoList.length ? (
+          todoList.map((data, i) => {
+            return <Card key={i} data={data} />;
+          })
+        ) : (
+          <div className={styles.empty}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/158/158398.png"
+              alt="sad"
+            />
+            <h2>No todos yet</h2>
+          </div>
+        )
       ) : (
-        <h2 className={styles.empty}>No todos yet</h2>
+        <div className={styles.loginFor}>
+          <img
+            src="https://cdn.icon-icons.com/icons2/2518/PNG/512/login_icon_151220.png"
+            alt="login-icon"
+          />
+          <span>Login to add todos</span>
+        </div>
       )}
     </>
   );
